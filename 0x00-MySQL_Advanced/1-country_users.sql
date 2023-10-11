090d6726dc1c145a0e3b5c907d934470a23dec83
Pinerealm <osalami09@gmail.com>
2023-10-12 02:17:31 +0300

    Add an ENUM type column

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+-- Create a table named users with the following requirements:
+-- id: integer, auto increment, primary key
+-- email: string (255 chars), never null, unique
+-- name: string (255 chars)
+-- country: enumeration of the countries US, CO and TN, never null, default US
+-- If the table already exists, do not throw an error.
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
