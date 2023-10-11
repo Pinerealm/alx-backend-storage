e180661c9f53bfae5f3d8466c6e0ce2d2a3c7998
Pinerealm <osalami09@gmail.com>
2023-10-12 02:15:59 +0300

    Create a table with constraints

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Create a table named users with the following attributes:
+-- id: integer, neevr null, auto increment and primary key
+-- email: string (255 chars), never null, unique
+-- name: string (255 chars)
+-- If the table already exists, do not throw an error.
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);
