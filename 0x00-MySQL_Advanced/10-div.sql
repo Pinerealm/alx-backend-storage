52ff278deb2280a5db76ac9c6ce594e403d6a3ed
Pinerealm <osalami09@gmail.com>
2023-10-15 01:36:32 +0300

    Create the function SafeDiv

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- Create a function, SafeDiv, that divides the first by the
+-- second number. If the second number is 0, return 0.
+DELIMITER && ;
+DROP FUNCTION IF EXISTS SafeDiv&&
+CREATE FUNCTION SafeDiv (a int, b int) RETURNS VARCHAR(255)
+    BEGIN
+        DECLARE res1 INT DEFAULT 0;
+        DECLARE res2 DOUBLE;
+        IF b = 0 THEN
+            RETURN CONCAT(res1);
+        ELSE
+            IF a % b = 0 THEN
+                SET res1 = a / b;
+                RETURN CONCAT(res1);
+            ELSE
+                SET res2 = a / CAST(b AS DECIMAL);
+                RETURN CONCAT(res2);
+            END IF;
+        END IF;
+    END&&
