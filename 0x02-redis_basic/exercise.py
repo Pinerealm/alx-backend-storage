650d516b532a83d72e1eede51e46d59713ab3300
Pinerealm <osalami09@gmail.com>
2023-10-19 01:38:08 +0300

    Implement the count_calls decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,23 @@
 import redis
 from typing import Union
 from uuid import uuid4
+from functools import wraps
 
 Redis_type = Union[str, bytes, int, float]
+
+
+def count_calls(method: callable) -> callable:
+    """Count the number of times a method is called
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -16,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Redis_type) -> str:
         """Generate a random key, store the input data in Redis using the key
         """
@@ -23,7 +39,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: callable = None) -> Redis_type:
+    def get(self, key: str, fn: callable = None) -> bytes:
         """Get the value stored in Redis for a given key
         """
         value = self._redis.get(key)
