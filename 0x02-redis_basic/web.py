6034945652506fbf60493bc767e5fc425d4ce58e
Pinerealm <osalami09@gmail.com>
2023-10-21 09:23:56 +0300

    Implement an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Implement an expiring web cache and tracker"""
+import redis
+import requests
+
+
+def get_page(url: str) -> str:
+    """Get the HTML content of a particular URL and return it
+    """
+    key = "count:{}".format(url)
+    r = redis.Redis()
+    r.incr(key)
+    cache = r.get(url)
+    if cache:
+        return cache.decode("utf-8")
+    else:
+        html = requests.get(url).text
+        r.setex(url, 10, html)
+        return html
