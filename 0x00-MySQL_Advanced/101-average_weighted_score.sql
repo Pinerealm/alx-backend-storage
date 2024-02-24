ad4248b1af2844608b124e7ae43cf4e11ca15153
Pinerealm <osalami09@gmail.com>
2024-02-24 04:44:00 +0300

    Remove call to compute average weighted score and add code logic directly

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,11 +1,15 @@
 -- Create a stored procedure, ComputeAverageWeightedScoreForUsers, that
 -- computes and stores the average weighted score for all students.
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers $$
 CREATE
     PROCEDURE ComputeAverageWeightedScoreForUsers ()
     BEGIN
         DECLARE done INT DEFAULT FALSE;
         DECLARE user_id INT;
+        DECLARE total_score FLOAT DEFAULT 0;
+        DECLARE total_weight INT DEFAULT 0;
+        DECLARE avg_score FLOAT;
         DECLARE cur CURSOR FOR SELECT id FROM users;
         DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
         
@@ -17,7 +21,17 @@ CREATE
                 LEAVE read_loop;
             END IF;
             
-            CALL ComputeAverageWeightedScoreForUser(user_id);
+            SELECT SUM(score * weight), SUM(weight)
+            INTO total_score, total_weight
+            FROM corrections AS c
+            JOIN projects AS p ON c.project_id = p.id
+            WHERE c.user_id = user_id;
+            
+            SET avg_score = total_score / total_weight;
+            
+            UPDATE users
+            SET average_score = avg_score
+            WHERE id = user_id;
         END LOOP;
         
         CLOSE cur;
