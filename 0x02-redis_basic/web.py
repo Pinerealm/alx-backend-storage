884efd315624b09d8729e6dbca0359bc1483846a
Pinerealm <osalami09@gmail.com>
2024-04-04 15:03:18 +0300

    Use the str function to convert cache from bytes

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,14 +5,15 @@ import requests
 
 
 def get_page(url: str) -> str:
-    """Get the HTML content of a particular URL and return it
+    """Gets the HTML content of a particular URL and return it. Return the
+    cached content if recently requested (up to 10 seconds).
     """
     key = "count:{}".format(url)
     r = redis.Redis()
     r.incr(key)
     cache = r.get(url)
     if cache:
-        return cache.decode("utf-8")
+        return str(cache)
     else:
         html = requests.get(url).text
         r.set(url, html, ex=10)
